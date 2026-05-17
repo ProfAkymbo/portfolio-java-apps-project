ea570817c9b6cce4752025b96e4f3edd63966930
ProfAkymbo <oyebanjiakeem@gmail.com>
2026-05-17 16:01:16 +0100

    add locking to s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "ak-backend-tf-bucket"
     key            = "app-cicd/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locking"
+    #dynamodb_table = "terraform-locking"
+    use_lockfile   = true
   }
 }
